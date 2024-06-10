7fb151c59e14e8793eafe1834ab2b9ee425703ee
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-10 18:55:04 +0200

    Refactor MoviesGrid

--- moviedb/src/components/MoviesGrid.js
+++ moviedb/src/components/MoviesGrid.js
@@ -1,34 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import '../styles.css';
+import MovieCard from './MovieCard';
 
 export default function MoviesGrid() {
-
   const [moviesList, setMoviesList] = useState([]);
 
   useEffect(() => {
-    fetch('movies.json')
-    .then(response => response.json())
-    .then(data => {
-      setMoviesList(data);
-    });
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setMoviesList(data);
+      });
   }, []);
 
   return (
-    <div className='movies-grid'>
-        {
-            moviesList.map((movie) => {
-                return (
-                    <div key={movie.id} className='movie-card'>
-                    <img src={`images/${movie.img}`} alt={movie.title} />
-                    <div className='movie-card-info'>
-                        <h3 className='movie-card-title'>{movie.title}</h3>
-                        <p className='movie-card-genre'>{movie.genre}</p>
-                        <p className='movie-card-rating'>{movie.rating}</p>
-                    </div>
-                    </div>
-                )
-            })
-        }
+    <div className="movies-grid">
+      {moviesList.map((movie) => {
+        return <MovieCard movie={movie}></MovieCard>;
+      })}
     </div>
   );
 }
