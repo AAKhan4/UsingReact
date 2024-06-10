9278ca52498adc6025397233333af94274559599
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-11 00:01:31 +0200

    Update MovieCard.js

--- moviedb/src/components/MovieCard.js
+++ moviedb/src/components/MovieCard.js
@@ -4,7 +4,7 @@ import '../styles.css';
 export default function MovieCard({ movie }) {
   return (
     <div key={movie.id} className="movie-card">
-      <img src={`images/${movie.img}`} alt={movie.title} />
+      <img src={`images/${movie.image}`} alt={movie.title} />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
