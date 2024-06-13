1d1e767307d2bc1133175401811a556294035eb5
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-14 00:44:54 +0200

    Update TicketForm.js

--- bug-reporter/src/components/TicketForm.js
+++ bug-reporter/src/components/TicketForm.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function TicketForm({ dispatch }) {
+export default function TicketForm({ dispatch, editingTicket }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("1");
+
+  useEffect(() => {
+    if (editingTicket) {
+      setTitle(editingTicket.title);
+      setDescription(editingTicket.description);
+      setPriority(editingTicket.priority);
+    } else {
+      clearForm();
+    }
+  }, [editingTicket]);
 
   const prorities = {
     1: "low",
@@ -21,13 +31,16 @@ export default function TicketForm({ dispatch }) {
     event.preventDefault();
 
     const ticket = {
-      id: new Date().toISOString(),
+      id: editingTicket ? editingTicket.id : new Date().toISOString(),
       title,
       description,
       priority,
     };
 
-    dispatch({ type: "ADD_TICKET", payload: ticket });
+    dispatch({
+      type: editingTicket ? "UPDATE_TICKET" : "ADD_TICKET",
+      payload: ticket,
+    });
 
     clearForm();
   };
