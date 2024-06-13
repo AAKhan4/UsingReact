d4506b99199ee3871a17f6f57459bf5f593e827e
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-14 00:32:33 +0200

    Update App.js

--- bug-reporter/src/App.js
+++ bug-reporter/src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
 import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
 
 function App() {
   const initState = {
@@ -17,6 +18,13 @@ function App() {
       <div className="container">
         <h1>Bug Report</h1>
         <TicketForm dispatch={dispatch} />
+
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+            <TicketList tickets={state.tickets} dispatch={dispatch} />
+          </div>
+        )}
       </div>
     </div>
   );
