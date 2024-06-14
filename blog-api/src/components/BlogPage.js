7c0f84b99dd5727d8d2e92052210286ea6f4263e
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-14 12:29:14 +0200

    Create BlogPage.js

---
+++ blog-api/src/components/BlogPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post";
+import { useContext } from "react";
+import { UserInfoContext } from "../context/UserInfoContext";
+
+export default function BlogPage() {
+  const userInfo = useContext(UserInfoContext);
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <Post username={userInfo.username} isAdmin={userInfo.isAdmin} />
+    </div>
+  );
+}
