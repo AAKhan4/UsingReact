cc9090aa62511d81e7876a072f57be37cdd0cd99
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-14 12:32:21 +0200

    Update App.js

--- blog-api/src/App.js
+++ blog-api/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import UserInfoContext from "./context/UserInfoContext";
+import { UserInfoContext } from "./context/UserInfoContext";
 import BlogPage from "./components/BlogPage";
 
 function App() {
