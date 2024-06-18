176c3f3b7d0bf8b199ce670fbfe867d2d2be954e
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-18 19:03:28 +0200

    create demo react api

---
+++ demo-api/src/components/Posts.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { getPosts } from '../services/postService';
+
+export default function Posts() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        console.log("Fetching posts");
+    });
+
+    return (
+        <div>
+            <h1>Posts</h1>
+        </div>
+    );
+}
