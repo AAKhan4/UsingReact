6912c36a8283010bcbfab6a1cd65d642561b1a0d
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-20 18:17:21 +0200

    Update Posts.js

--- demo-api/src/components/Posts.js
+++ demo-api/src/components/Posts.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { getPosts, deletePost } from "../services/postService";
+import { getPosts, deletePost, updatePost } from "../services/postService";
 import PostForm from "./PostForm";
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
+  const [editing, setEditing] = useState(null);
+
   useEffect(() => {
     getPosts()
       .then((response) => {
@@ -24,15 +26,25 @@ export default function Posts() {
       });
   };
 
+  const editPost = (post) => {
+    setEditing(post);
+  };
+
   return (
     <div>
       <h1>Posts</h1>
-      <PostForm posts={posts} setPosts={setPosts} />
+      <PostForm
+        posts={posts}
+        setPosts={setPosts}
+        editing={editing}
+        setEditing={setEditing}
+      />
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
+            <button onClick={() => editPost(post)}>Edit</button>
             <button onClick={() => handleDelete(post.id)}>Delete</button>
           </li>
         ))}
