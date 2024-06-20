5115e910d86cf8165615c4a3ddc6032dd70a26e7
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-20 18:04:59 +0200

    Create PostForm.js

---
+++ demo-api/src/components/PostForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { createPost } from "../services/postService";
+
+export default function PostForm(posts, setPosts) {
+  const { title, setTitle } = useState("");
+  const { body, setBody } = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createPost({ title, body })
+      .then((response) => {
+        setPosts([...posts, response.data]);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>Title</div>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <div>Body</div>
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+        <button type="submit">Add Post</button>
+      </form>
+    </div>
+  );
+}
